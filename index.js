4e0e1fada8e1a6a831c5b15642223e1021506318
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-09-15 14:29:58 +0700

    feat: implement client side rendering

--- index.js
+++ index.js
@@ -5,17 +5,47 @@ const root = document.getElementById("root");
 linkHome.onclick = function (event) {
   event.preventDefault();
   history.pushState(null, "", event.target.href);
-  root.textContent = "Welcome to Home Page";
+  root.innerHTML = "";
+  root.appendChild(HomePage());
 };
 
 linkAbout.onclick = function (event) {
   event.preventDefault();
   history.pushState(null, "", event.target.href);
-  root.textContent = "Welcome to About Page";
+  root.innerHTML = "";
+  root.appendChild(AboutPage());
 };
 
+function HomePage() {
+  const p = document.createElement("p");
+  p.textContent = "Welcome to Home Page";
+
+  const textPreview = document.createElement("p");
+
+  const input = document.createElement("input");
+  input.placeholder = "enter your name";
+  input.oninput = function (event) {
+    textPreview.textContent = event.target.value;
+  };
+
+  const div = document.createElement("div");
+  div.append(p);
+  div.append(input);
+  div.append(textPreview);
+
+  return div;
+}
+
+function AboutPage() {
+  const p = document.createElement("p");
+  p.textContent = "Welcome to About Page";
+  return p;
+}
+
 if (window.location.pathname == "/") {
-  root.textContent = "Welcome to Home Page";
+  root.innerHTML = "";
+  root.appendChild(HomePage());
 } else if (window.location.pathname == "/about") {
-  root.textContent = "Welcome to About Page";
+  root.innerHTML = "";
+  root.appendChild(AboutPage());
 }
